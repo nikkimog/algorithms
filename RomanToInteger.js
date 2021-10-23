d8baabcbedd26461d7845739cb7dd97b3c6888ce
Nikki Mogensen <nicolerosemogensen@gmail.com>
2021-10-23 08:34:41 -0400

    new roman int solutioN

--- RomanToInteger.js
+++ RomanToInteger.js
@@ -75,3 +75,35 @@ var romanToInt = function (s) {
 	}
 	return solution;
 };
+
+// best solution.. you don't have to turn it into an array you can loop through strings sillly
+
+let romanDictionary = {
+	I: 1,
+	IV: 4,
+	V: 5,
+	IX: 9,
+	X: 10,
+	XL: 40,
+	L: 50,
+	XC: 90,
+	C: 100,
+	CD: 400,
+	D: 500,
+	CM: 900,
+	M: 1000,
+};
+var romanToInt = function (s) {
+	let romanInt = 0;
+	for (let i = 0; i < s.length; i++) {
+		let currChar = s[i];
+		let nextChar = s[i + 1];
+		if (romanDictionary[currChar + nextChar]) {
+			romanInt += romanDictionary[currChar + nextChar];
+			i++;
+		} else {
+			romanInt += romanDictionary[currChar];
+		}
+	}
+	return romanInt;
+};
